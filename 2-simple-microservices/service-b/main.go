89ead7c25023b872836fcda66b4b29f247fbc62f
Sven Söhnlein <73952675+svensoehnlein@users.noreply.github.com>
2024-11-13 11:53:03 +0100

    Update main.go

--- 2-simple-microservices/service-b/main.go
+++ 2-simple-microservices/service-b/main.go
@@ -1,1 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Service B!")
+}
+
+func main() {
+	http.HandleFunc("/service-b", handler)
+	log.Println("Service B listening on port 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
