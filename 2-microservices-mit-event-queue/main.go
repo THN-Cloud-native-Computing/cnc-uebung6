7c7204340b0bc6ac6f22df06c8a4b53d0fca2750
Sven Söhnlein <73952675+svensoehnlein@users.noreply.github.com>
2024-11-13 12:34:30 +0100

    Update main.go

--- 2-microservices-mit-event-queue/main.go
+++ 2-microservices-mit-event-queue/main.go
@@ -1,1 +1,15 @@
+package main
+
+import (
+    "project/queue"
+    "project/serviceA"
+    "project/serviceB"
+)
+
+func main() {
+    mq := queue.NewMessageQueue(10)
+
+    go serviceA.StartServiceA(mq)
+    serviceB.StartServiceB(mq)
+}
 
