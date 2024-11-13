a261757b75a429ee62c4d69b7f86638d27f64cf1
Sven Söhnlein <73952675+svensoehnlein@users.noreply.github.com>
2024-11-13 11:51:58 +0100

    Update main.go

--- 2-simple-microservices/service-a/main.go
+++ 2-simple-microservices/service-a/main.go
@@ -1,1 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://localhost:8081/service-b")
+	if err != nil {
+		http.Error(w, "Error calling Service B", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Error reading response from Service B", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Response from Service B: %s", string(body))
+}
+
+func main() {
+	http.HandleFunc("/service-a", handler)
+	log.Println("Service A listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
